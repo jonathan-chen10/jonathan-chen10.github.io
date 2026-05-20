dd241c6c34c9bbd3afbf6b10b069a0d6f484125d
Jonathan Chen <jonafish10@gmail.com>
2026-05-20 18:16:35 -0400

    Implement slugify

--- blog-generator/src/parse.rs
+++ blog-generator/src/parse.rs
@@ -205,5 +205,12 @@ fn tree_to_li(tree: &TocNode) -> String {
 }
 
 fn slugify(s: &str) -> String {
-    s.to_string()
+    s.to_lowercase()
+        .chars()
+        .map(|c| if c.is_alphanumeric() { c } else { '-' })
+        .collect::<String>()
+        .split('-')
+        .filter(|s| !s.is_empty())
+        .collect::<Vec<_>>()
+        .join("-")
 }
